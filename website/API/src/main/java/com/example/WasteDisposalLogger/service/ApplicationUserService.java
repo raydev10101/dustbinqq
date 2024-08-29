c7c87fbb285c810ac77f98aa0e2859484df2b454
raydev10101 <139460234+raydev10101@users.noreply.github.com>
2024-08-29 09:34:38 -0500

    Add files via upload
    Heres the program code

---
+++ website/API/src/main/java/com/example/WasteDisposalLogger/service/ApplicationUserService.java
@@ -0,0 +1,34 @@
+package com.example.WasteDisposalLogger.service;
+
+import com.example.WasteDisposalLogger.dto.Login;
+import com.example.WasteDisposalLogger.dto.UserDto;
+import com.example.WasteDisposalLogger.entity.ApplicationUser;
+import com.example.WasteDisposalLogger.repository.ApplicationUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+@RequiredArgsConstructor
+public class ApplicationUserService {
+    private final ApplicationUserRepository applicationUserRepository;
+
+
+    public int signUp(UserDto userDto){
+        Optional<ApplicationUser> optionalApplicationUser = applicationUserRepository.findByUsername(userDto.getUsername());
+        if (optionalApplicationUser.isEmpty()){
+            applicationUserRepository.save(ApplicationUser.builder()
+                    .phoneNumber(userDto.getPhoneNumber())
+                    .password(userDto.getPassword())
+                    .username(userDto.getUsername())
+                    .build());
+            return 200;
+        }
+        return HttpStatus.CONFLICT.value();
+    }
+    public ApplicationUser login(Login login){
+        Optional<ApplicationUser> optionalApplicationUser = applicationUserRepository.findByUsernameAndPassword(login.getUsername(),login.getPassword());
+        return optionalApplicationUser.orElse(null);
+    }
+}
